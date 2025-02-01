bd657e98936de40137c073390a5de3f5c761f216
jhonomar <jhonomar109@gmail.com>
2025-02-01 10:27:52 -0500

    Agregado formulario de guardar cliente y ajustes en validaciones

--- src/main/java/mx/com/gm/web/WebConfig.java
+++ src/main/java/mx/com/gm/web/WebConfig.java
@@ -6,30 +6,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
-    
+public class WebConfig implements WebMvcConfigurer {
     @Bean
-    public LocaleResolver localeResolver(){
-        var slr = new SessionLocaleResolver();
-        //slr.setDefaultLocale(new Locale("es"));
-        slr.setDefaultLocale(Locale.forLanguageTag("es"));
-        return slr;
+    public LocaleResolver localeResolver() {
+        var resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(Locale.forLanguageTag("es"));
+        resolver.setCookieName("lang");
+        resolver.setCookieMaxAge(3600); // Duración de la cookie en segundos
+        return resolver;
     }
-    
+
     @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor(){
+    public LocaleChangeInterceptor localeChangeInterceptor() {
         var lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
         return lci;
     }
-    
+
     @Override
-    public void addInterceptors(InterceptorRegistry registro){
+    public void addInterceptors(InterceptorRegistry registro) {
         registro.addInterceptor(localeChangeInterceptor());
     }
-    
+
 }
